8fe4952b930f482c07afde2fc2d58309b8d49317
maanyamadan0101 <maanyamadan0101@gmail.com>
2026-08-10 19:30:41 +0530

    Improve lead conversion and local business attribution

--- src/data/site.ts
+++ src/data/site.ts
@@ -16,6 +16,9 @@ export const CONTACT = {
   website: "shubhestatebroker.in",
   googleBusinessProfile:
     "https://www.google.com/search?q=Shubh+Estate+Brokers&stick=H4sIAAAAAAAA_-NgU1I1qDC2NEgxMjZJSjS1SDNJSjO1AgqlJKYZpBqYpSUlG5qbp5ouYhUJzihNylBwLS5JLElVcCrKz04tKgYAC6kHGEAAAAA&hl=en-GB&mat=CQc4_erXssgBElYBa0lj_ybgmqVIZrB49UjyibAzFmZh9OK8qFLi_nYKDltSnbMrwIBsFTUjH8wfBsu3gyXxLwV5zSI2FA9iRh2qJnxzRfuDGDzpEFaMfMJGQspwRNUuWw",
+  googleReview: "https://g.page/r/CeV3wfsGDq89EBM/review",
+  instagram: "https://www.instagram.com/shubhestatebrokers/",
+  youtube: "https://www.youtube.com/channel/UCMryfMFC6_8fBP50_-WOLUA",
 };
 
 export const LOAN_DISCLAIMER =
