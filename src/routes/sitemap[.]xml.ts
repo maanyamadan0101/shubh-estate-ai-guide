b092fd7252a08b46ad5ac8a1a21fea0adaaae492
maanyamadan0101 <maanyamadan0101@gmail.com>
2026-08-23 11:08:47 +0530

    Keep sitemap escaping unchanged

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -79,7 +79,7 @@ function escapeXml(value: string) {
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/\"/g, "&quot;")
+    .replace(/"/g, "&quot;")
     .replace(/'/g, "&apos;");
 }
 
